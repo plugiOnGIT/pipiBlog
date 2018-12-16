873902ccd7515520f9c12db6bdc83b0a38b4b5ae
heyyihai <1479321513@qq.com>
2018-12-16 22:45:13 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  base: '/pipiBlog/',
+  title: 'pipiBlog',
+  description: ' ',
+  themeConfig: {
+  //   nav: [
+  //     {
+  //       text: '组件',
+  //       items: [
+  //         { text: 'comboGrid', link: '/comboGrid' },
+  //         { text: 'paginationTable', link: '/paginationTable' }
+  //       ]
+  //     },
+  //     { text: '公共方法', link: '/publicMethods' }
+  //   ],
+    sidebar: {
+      '/GO/': [
+        ['', 'GO']
+      ]
+    }
+  }
+}
